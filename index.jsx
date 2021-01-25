c6fe902bbbd65bfc6c26c4917e7541c8471258c8
munchkinhalfling <munchkinhalfling@pm.me>
2021-01-25 12:50:55 -0500

    Make it FAST

--- index.jsx
+++ index.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
+import Spinner from 'react-bootstrap/Spinner';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-ReactDOM.render(<App/>, document.querySelector("main"));
+const App = lazy(() => import('./components/App'));
+const loader = () => <Spinner animation="border" style={{marginLeft: "50%", marginRight: "50%", marginTop: "50%", marginBottom: "50%"}} />
+ReactDOM.render(<Suspense fallback={loader()}><App/></Suspense>, document.querySelector("main"));
