2f858533cf32f18210ebd5b2c730cb3e96cb0444
aleksrutins <alex@rutins.com>
2021-04-23 16:51:03 -0400

    Add value

--- components/Criticism.jsx
+++ components/Criticism.jsx
@@ -8,7 +8,7 @@ export default function Criticism() {
         </Jumbotron>
         <Media>
             <Image src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/dbeda9dc-630b-498d-ab67-17e16852a016/d3ll57k-6602af3e-5501-4309-9afb-eb10ed915cac.png/v1/fill/w_1192,h_670,q_75,strp/menger_sponge_variation_1_by_krzysztofmarczak-d3ll57k.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwic3ViIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS5vcGVyYXRpb25zIl0sIm9iaiI6W1t7InBhdGgiOiIvZi9kYmVkYTlkYy02MzBiLTQ5OGQtYWI2Ny0xN2UxNjg1MmEwMTYvZDNsbDU3ay02NjAyYWYzZS01NTAxLTQzMDktOWFmYi1lYjEwZWQ5MTVjYWMucG5nIiwid2lkdGgiOiI8PTExOTIiLCJoZWlnaHQiOiI8PTY3MCJ9XV19.iq6EgWI-zMIfJFm92KstjKDVNdg0AI7GBDGlQjkUGe8"
-                rounded height="256" className="mr-3" />
+                rounded width="455" className="mr-3" />
             <Media.Body>
                 <h5>Emotionalism</h5>
                 <p>
@@ -21,6 +21,15 @@ export default function Criticism() {
                 <h5>Lines</h5>
                 <p>
                     This picture is made up mostly of straight lines. There is shading and light as well, which is done by a computer, but still uses lines to some degree. 
+                </p>
+            </Media.Body>
+        </Media>
+        <Media className="mt-5">
+            <Image rounded width="455" className="mr-3" src="https://www.jdhillberry.com/images/New%20Man%20Behind%20the%20Artwork%20w.jpg"/>
+            <Media.Body>
+                <h5>Value</h5>
+                <p>
+                    Value is the tone, or lightness/darkness, of the image. In this image, the darker tones are used for shadows.
                 </p>
             </Media.Body>
         </Media>
